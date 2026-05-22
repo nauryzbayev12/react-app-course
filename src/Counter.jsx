ff71fa1ec63c2f968e553119a01f5c41034b8f9e
nauryzbaev_be <nauryzbaev_be@bi.group>
2026-05-22 11:45:32 +0500

    fix
    fix

---
+++ src/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Button } from './components/Button/Button'
+
+export const Counter = () => {
+	
+	const [count, setCount] = useState(0);
+
+
+	const setCounterHandler = () =>
+	{
+				setCount((prev) => prev + 1)
+				setCount((prev) => prev + 1)
+				setCount((prev) => prev + 1)
+	}
+
+	return (
+
+			<Button onClick={setCounterHandler}> count is {count} </Button>
+	);
+
+};
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import { } from 'react'
+import cls from './Button.module.css'
+
+const isPrimary = true;
+
+export const  Button = ({onClick , children}) => 
+{
+	//console.log(onClick , children);
+	return (
+		<button className = {`${cls.btn} ${isPrimary ? cls.primary :""}`} onClick={onClick}  > 
+		{children} </button>
+	);
+	
+}
