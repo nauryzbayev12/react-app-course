8c9145850f897e4612aa64038e24b07cf9feed11
nauryzbaev_be <nauryzbaev_be@bi.group>
2026-05-22 11:59:01 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,48 @@
-import js from '@eslint/js'
-import prettier from 'eslint-plugin-prettier": "^5.5.5'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
+import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
     },
-    plugins:
-    {
-      prettier
+    settings: { react: { version: "18.3" } },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier,
     },
-    rules : 
-    {
-      "prettier/prettier" : 
-      [
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-no-target-blank": "warn",
+      "react/prop-types": "warn",
+      "react/display-name": "off",
+      "no-console": "warn",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "prettier/prettier": [
         "error",
         {
-          singleQuote : false,
+          singleQuote: false,
           printWidth: 130,
-          tabWidth: 2
+          tabWidth: 2,
         },
-      ]
-    }
+      ],
+    },
   },
-])
+];
