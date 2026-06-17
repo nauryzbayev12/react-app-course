bc38a13cd897425bdc5cb8b4904c5e93fecc2872
nauryzbaev_be <nauryzbaev_be@bi.group>
2026-06-17 16:09:38 +0500

    Update AddQuestionPage.jsx

--- src/Pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/Pages/AddQuestionPage/AddQuestionPage.jsx
@@ -8,13 +8,67 @@ export const AddQuestionPage = () =>
 		<h1 className= {cls.fromTitle}>Add new question</h1>
 		<div className={cls.fromContainer}>
 			<form action="" className={cls.form}>
-				<label htmlFor="questionField">Question: </label>
-				<textarea 
-					name="" id="questionField"
-				 	cols="30"
-					 rows="2"
-					required
-					placeholder="pleace enter a placeholder"></textarea>
+				<div className={cls.formControl}>
+					<label htmlFor="questionField">Question: </label>
+					<textarea 
+						defaultValue={"defaultValue"}
+						name="question"
+						id="questionField"
+						cols="30"
+						rows="2"
+						required
+						placeholder="pleace enter a placeholder">
+					</textarea>
+				</div>
+				<div className={cls.formControl}>
+					<label htmlFor="answerField">Short answer: </label>
+					<textarea 
+						defaultValue={"defaultValue"}
+						name="answer"
+						id="AnswerField"
+						cols="30"
+						rows="2"
+						required
+						placeholder="pleace enter a short answer">
+					</textarea>
+				</div>
+				<div className={cls.formControl}>
+					<label htmlFor="descriptionField">Description: </label>
+					<textarea 
+						defaultValue={"defaultValue"}
+						name="description"
+						id="descriptionField"
+						cols="30"
+						rows="5"
+						required
+						placeholder="pleace enter a description">
+					</textarea>
+				</div>
+				<div className={cls.formControl}>
+					<label htmlFor="resourcesField">Resources: </label>
+					<textarea 
+						defaultValue={"defaultValue"}
+						name="resources"
+						id="resourcesField"
+						cols="30"
+						rows="1"
+						required
+						placeholder="pleace enter resources separated by comas">
+					</textarea>
+				</div>
+				<div className={cls.formControl}>
+					<label htmlFor="levelField" >Level: </label>
+					<select name="level" id="levelField" 	defaultValue={"defaultValue"}> 
+						<option disabled>Question level</option>
+						<hr/>
+						<option value="10">1 - easiest</option>
+						<option value="20">2 - madium</option>
+						<option value="30">3 - hardest</option>
+					</select>
+				</div>
+				<label htmlFor="clearFormField" className = {cls.clearFormField}>
+					<input className={cls.checkbox} type="checkbox" name="clearForm" id ="clearFormField" 	defaultValue={true} />
+				</label>
 			</form>
 		</div>
 	</>;
